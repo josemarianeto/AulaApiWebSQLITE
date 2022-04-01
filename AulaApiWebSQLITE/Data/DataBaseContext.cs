af4e83a1be781af02ba1d30a6ea2801a42348edc
José Maria Santana de Melo Neto <42035282+josemarianeto@users.noreply.github.com>
2022-04-01 15:01:56 -0300

    first commit

---
+++ AulaApiWebSQLITE/Data/DataBaseContext.cs
@@ -0,0 +1,15 @@
+﻿using AulaApiWebSQLITE.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace AulaApiWebSQLITE.Data
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ AulaApiWebSQLITE/Program.cs
@@ -0,0 +1,30 @@
+using AulaApiWebSQLITE.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DataBaseContext>(options => options.UseSqlite("Data Source = ./DB/aulawebapi.db"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
